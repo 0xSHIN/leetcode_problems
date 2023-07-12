a052d0f2bf410f05c7f058bc73e3573ac9338542
SHIN <126964670+cyberintrusion@users.noreply.github.com>
2023-07-12 08:04:14 +0200

    repeatedCharacter function

--- repeatedCharacter.c
+++ repeatedCharacter.c
@@ -1,16 +1,13 @@
-#include <stdbool.h>
-
-char repeatedCharacter(char *s) {
-    bool visited[128] = {false};  // Initialize a "hash table" for ASCII characters
-
+char repeatedCharacter(char *s)
+{
+    bool visited[128] = {false};
     int i = 0;
-    while (s[i] != '\0') {
-        if (visited[(int)s[i]]) {  // If the character was already visited
-            return s[i];  // Return the repeated character
-        }
-        visited[(int)s[i]] = true;  // Mark this character as visited
+    while (s[i] != '\0')
+    {
+        if (visited[(int)s[i]])
+            return s[i];
+        visited[(int)s[i]] = true;
         i++;
     }
-
-    return '\0';  // Return null character if no repeated character is found
+    return ('\0');
 }
